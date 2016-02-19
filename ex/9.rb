8c32d2ab9994c439b6f50b60eefd6de8e85d47b0
Josh Frankel <joshmfrankel@gmail.com>
2016-02-19 08:41:13 -0500

    Commit all

---
+++ ex/9.rb
@@ -0,0 +1,20 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There's something going on here.
+Withthe three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
+puts "yo yo yo" "dawgg I'm exibit"
+
+puts "
+Line 1
+Line 2
+Line 3
+"
